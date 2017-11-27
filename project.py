f4177702f60a85c619af5af8e152c1c1ac7a030c
Amitoj Gill <amitojgill@Amitojs-MacBook-Pro.local>
2017-11-27 14:43:16 -0800

    added editMenuItem functionality

--- project.py
+++ project.py
@@ -22,7 +22,7 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
 	if request.method == 'POST':
-		newItem = MenuItem(name=request.form['name'],restaurant_id=restaurant_id)
+		newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
 		session.add(newItem)
 		session.commit()
 		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
@@ -30,10 +30,18 @@ def newMenuItem(restaurant_id):
 		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
-
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET','POST'])
 def editMenuItem(restaurant_id, menu_id):
-	return "Page to edit a new menu item. Task 2 complete!"
+	editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+	if request.method == 'POST':
+		if request.form['name']:
+			editedItem.name = request.form['name']
+		session.add(editedItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
+	else:
+		return render_template('editmenuitem.html',restaurant_id=restaurant_id, menu_id = menu_id, item=editedItem)
+
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
