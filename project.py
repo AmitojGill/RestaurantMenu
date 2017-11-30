4cb4ed83723087a9852643ed2515da543a70d8db
Amitoj Gill <amitojgill@Amitojs-MacBook-Pro.local>
2017-11-30 15:32:22 -0800

    returnMenuJSON added:

--- project.py
+++ project.py
@@ -1,5 +1,4 @@
-from flask import Flask , render_template, request, redirect, url_for
-from flask import flash
+from flask import Flask , render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -12,6 +11,13 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+#Making an API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+	return jsonify(MenuItem=[i.serialize for i in items])
 
 
 @app.route('/')
