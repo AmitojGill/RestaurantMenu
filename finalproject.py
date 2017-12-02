95df998d4f997355ee7a3d9cf877db8f3650179a
Amitoj Gill <amitojgill@Amitojs-MacBook-Pro.local>
2017-12-02 04:56:50 -0800

    Restaurnat.db connected

--- finalproject.py
+++ finalproject.py
@@ -1,25 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
 
+'''
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
 restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 
 #Fake Menu Items
 items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
+'''
 
 #Show all restaurants
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-    return render_template('restaurants.html', restaurants = restaurants)
+	restaurants = session.query(Restaurant)
+	return render_template('restaurants.html', restaurants = restaurants)
 	
 
 #Add a new restaurant
