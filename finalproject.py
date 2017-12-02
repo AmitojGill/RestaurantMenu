4e0550989c039153c051ca5af4f178b620fa835f
Amitoj Gill <amitojgill@Amitojs-MacBook-Pro.local>
2017-12-02 05:01:55 -0800

    Configure showMenu function with database

--- finalproject.py
+++ finalproject.py
@@ -49,6 +49,8 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
 	return render_template('menu.html',items = items, restaurant=restaurant)
 
 #Add a new menu item
