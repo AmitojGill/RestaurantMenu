0b07d95a41ad2fc9ee757ddb127c2bc4be2b27f8
Amitoj Gill <amitojgill@Amitojs-MacBook-Pro.local>
2017-12-03 07:01:46 -0800

    Add Secret_key

--- finalproject.py
+++ finalproject.py
@@ -136,5 +136,6 @@ def deleteMenuItem(restaurant_id,menu_id):
 		return render_template('deletemenuitem.html',restaurant = restaurant, item = itemToDelete)
 
 if __name__ == '__main__':
+	app.secret_key = 'super_secret_key'
 	app.debug = True
 	app.run(host='0.0.0.0', port=5000)
